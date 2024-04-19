247c64b64aeabfc7a85a60faad1e822fdb473b6f
zygomaticontheway <mrzygomatic@gmail.com>
2024-04-19 13:45:57 +0200

    Lesson14 css

--- Lesson14/wine/wine.js
+++ Lesson14/wine/wine.js
@@ -8,30 +8,32 @@
 
 const container = document.querySelector(".container")  //.querySelector(".container") поиск элемента по какому-то свойству, в нашем случае это class ибо пишем в скобках через точку
 //.getElementsByClassName("container") аналогично тут применим, только в скобках пишем без точки
-fetch("https://api.sampleapis.com/wines/whites")
+fetch("https://api.sampleapis.com/wines/reds")
     .then(res => res.json())
     .then(json => render(json))
 
 const render = array => {
     array
         .filter(el => el.rating.average >= 4.8) // ключ выглядит так "rating": {"average": "4.9", "reviews": "37 ratings"},
-        .forEach(({image, wine, winery, rating}) => { //деструктуризация = получение ключей и значений из API
+        .forEach(({image, wine, winery, location, rating}) => { //деструктуризация = получение ключей и значений из API
             const cardElem = document.createElement("div");
             const titleElem = document.createElement("p");
             const wineryElem = document.createElement("p");
             const ratingElem = document.createElement("p");
+            const locationgElem = document.createElement("p");
             const imgElem = document.createElement("img");
 
             titleElem.innerText = `Title: ${wine}`;
             wineryElem.innerText = `Winery: ${winery}`;
             ratingElem.innerText = `Rating: ${rating.average}`;
+            locationgElem.innerText = `From: ${location}`
             
             imgElem.src = image;
             imgElem.alt = wine;
 
             cardElem.style.backgroundColor = rating.average >4.8 ? "lightgreen" : "lightblue";
 
-            cardElem.append(imgElem, titleElem, wineryElem, ratingElem);
+            cardElem.append(imgElem, titleElem, wineryElem, ratingElem, locationgElem);
             container.append(cardElem);
         })  
 }
