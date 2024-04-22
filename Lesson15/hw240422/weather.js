43c7cccb4ae878671438b181cf1e099f0f3b20ff
zygomaticontheway <mrzygomatic@gmail.com>
2024-04-23 00:53:18 +0200

    hw release 1 without readme

--- Lesson15/hw240422/weather.js
+++ Lesson15/hw240422/weather.js
@@ -1,4 +1,4 @@
-const containerElt = document.querySelector(".container");
+const containerElt = document.getElementById("container");
 const ip = "176.199.208.229";
 
 async function geojs() {
@@ -7,28 +7,34 @@ async function geojs() {
     console.log(geoObj);
     const {latitude, longitude, city, country, region} = geoObj;
     console.log(latitude, longitude, country, city, region);
-    
+
     const regionContainerElt = document.createElement ("div");
     regionContainerElt.className = "regionContainer";
+    regionContainerElt.innerText = `${country}/${region}`
 
-    const cityContainerElt = document.createElement ("div");
-    cityContainerElt.className = "cityContainer";
+    containerElt.append(regionContainerElt);
 
-    const countryElt = document.createElement ("span");
-    const regionElt = document.createElement ("span");
-    const cityElt = document.createElement ("span");
-
-    regionContainerElt.append (countryElt, regionElt);
     
-    cityContainerElt.append (cityElt);
 
-    weather(geoObj);
+    weather(latitude, longitude, city);
+
 }
 
-async function weather (){
+async function weather (latitude, longitude, city){
     const response = await fetch(`https://api.open-meteo.com/v1/dwd-icon?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,is_day,precipitation,weather_code&hourly=is_day&forecast_days=1`);
     const obj = await response.json();
     console.log(obj);
-    const {latitude, longitude, current, current_units} = obj;
+    const {current, current_units} = obj;
+
+    const cityContainerElt = document.createElement ("div");
+    cityContainerElt.className = "cityContainer";
+    cityContainerElt.innerText = `In ${city} is ${current.temperature_2m}${current_units.temperature_2m}`;
+
+    containerElt.append(cityContainerElt);
+
+
+    
+    
+
 }
 geojs()
